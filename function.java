aa068bd1612bcaa618a07cbf90f625618e3cd94b
Vaziyakarim <karimvaziya545@gmail.com>
2025-06-03 07:51:01 +0530

    second commit

--- function.java
+++ function.java
@@ -8,31 +8,6 @@ public class functions6{
     //     System.out.println("hello world");
     //     System.out.println("hello world");
     // }
-     //calculate sum
-    // public static void calsum(){
-    //     Scanner sc =new Scanner(System.in);
-
-    //      int a =sc.nextInt();
-    //      int b =sc.nextInt();
-    //      int sum=a+b;
-    //      System.out.println("the sum is:"+ sum);
-    // }
-
-    //function with parameters
-    // public static void calsum(int num1,int num2){     //parameters or formal parameters
-       
-    //      int sum=num1+num2;
-    //      System.out.println("the sum is:"+ sum);
-         
-    // }
-
-    //function with return type
-    // public static int calsum(int num1,int num2){
-       
-    //      int sum=num1+num2;
-    //      return sum;
-         
-    // }
     // public static void main(String[] args) {
     //     Scanner sc =new Scanner(System.in);
         // HelloWorld();
