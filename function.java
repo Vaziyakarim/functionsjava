4c2cff6fd1aeac625c01d53a2c940af0b3c675b1
Vaziyakarim <karimvaziya545@gmail.com>
2025-06-03 07:42:09 +0530

    first commit

---
+++ function.java
@@ -0,0 +1,10 @@
+//code for lecture number 6 -functions and methods
+ import java.util.Scanner;
+public class functions6{
+
+    //functions
+    // public static void HelloWorld(){
+    //     System.out.println("hello world");
+    //     System.out.println("hello world");
+    //     System.out.println("hello world");
+    // }
